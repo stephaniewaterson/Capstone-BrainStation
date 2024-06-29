ca78d00a2539daa192ef1984d66739df2b49d85a
Stephanie Waterson <stephaniemariewaterson@gmail.com>
2024-06-29 20:42:15 +0100

    create fetch locations function

--- src/pages/BlogHome/BlogHome.jsx
+++ src/pages/BlogHome/BlogHome.jsx
@@ -1,12 +1,35 @@
 import Header from "../../components/Header/Header";
+import fetchLocations from "../../utils/FetchLocations.jsx";
 import "./BlogHome.scss";
-
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
+import { BeatLoader } from "react-spinners";
 
 export default function BlogHome() {
+  const { id } = useParams();
+  const [location, setLocation] = useState(null);
+
+  const { locations, error } = fetchLocations();
+
+  if (!locations) {
+    return <BeatLoader color="#2e66e5" />;
+  }
+
+  if (error) {
+    return <p>Error fetching data: {error.message}</p>;
+  }
+
   return (
     <section className="home">
       <h2 className="home__header">Spanish</h2>
+      {locations.map((location) => (
+        <Link>
+          <article className="locationfinder">
+            <h3 className="locationfinder__header">{location.name}</h3>
+          </article>
+        </Link>
+      ))}
+
       <div className="home__section"></div>
     </section>
   );
---
+++ src/utils/FetchLocations.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function fetchLocations() {
+  const [locations, setLocations] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/locations`
+        );
+        setLocations(data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { locations, error };
+}
