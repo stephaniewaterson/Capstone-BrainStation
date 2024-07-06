379403664ad9edb52dbd30c0276d9d7516b38497
Stephanie Waterson <stephaniemariewaterson@gmail.com>
2024-07-06 22:21:25 +0100

    add comments input

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./BlogCard.scss";
 import axios from "axios";
+import addIcon from "../../assets/icons/add.png";
 
 export default function BlogCard({ post }) {
   const API_URL = import.meta.env.VITE_BACKEND_URL;
@@ -8,6 +9,7 @@ export default function BlogCard({ post }) {
   const date = new Date(post.timestamp);
   const [comments, setComments] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
   const fetchComments = async () => {
     try {
@@ -27,6 +29,36 @@ export default function BlogCard({ post }) {
     return <p>is loading...</p>;
   }
 
+  const handleAddPost = async (event) => {
+    event.preventDefault();
+
+    const post_id = post.id;
+    const name = event.target.name.value;
+    const comment = event.target.comment.value;
+
+    const result = {
+      post_id,
+      name,
+      comment,
+    };
+
+    if (name === "") {
+      name = "anonymous";
+    }
+
+    if (!comment) {
+      return;
+    }
+
+    try {
+      await axios.post(`${API_URL}/posts/${post.id}/comments`, result);
+      setFormSubmitted(true);
+      fetchComments();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <article className="post">
@@ -38,14 +70,37 @@ export default function BlogCard({ post }) {
           <p className="post__text">{post.content}</p>
         </div>
       </article>
-      {comments.map((comment) => {
-        return (
-          <article key={comment.id} className="comment">
-            <p>{comment.name}</p>
-            <p>{comment.comment}</p>
-          </article>
-        );
-      })}
+      <section className="post__comment">
+        <h3 className="post__comment-number">Comments</h3>
+        {comments.map((comment) => {
+          return (
+            <article key={comment.id} className="comment">
+              <p className="comment__name">{comment.name}</p>
+              <p className="comment__text">{comment.comment}</p>
+            </article>
+          );
+        })}
+        <form
+          className="comment-form"
+          onSubmit={(event) => {
+            handleAddPost(event);
+          }}
+        >
+          <input
+            className="comment-form__input-name"
+            type="text"
+            name="name"
+            placeholder="Name"
+          />
+          <input
+            className="comment-form__input"
+            type="text"
+            name="comment"
+            placeholder="Add comment"
+          />
+          <button alt="" className="comment-form__add-icon"></button>
+        </form>
+      </section>
     </>
   );
 }
