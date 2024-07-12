eaaeee9a4cb6eb155373577d3c78425bc2fa8897
Stephanie Waterson <stephaniemariewaterson@gmail.com>
2024-07-12 15:59:07 +0100

    add env variable for socekt

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 import ChatMessage from "../../components/ChatMessage/ChatMessage";
 import { Dropdown } from "primereact/dropdown";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect(import.meta.env.SOCKET_BACKEND_URL);
 
 export default function Chat({ user }) {
   const [username, setUsername] = useState("");
